64137c973038f8087a59ee1045e330d2abb690fc
Vlad_Alieksieievskyi <xr940kov@gmail.com>
2022-04-21 22:12:30 +0300

    Added The hollow square function

---
+++ Lesson 2/Task 2_1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter size of square:" << endl;
+    int sizeOfSquare = 0;
+    cin >> sizeOfSquare;
+    for (int i = 0; i < sizeOfSquare; i++){
+        for (int j = 1; j < sizeOfSquare; j++){
+            if (i == 0)
+                cout << "*";
+            else if (i == sizeOfSquare - 1)
+                cout << "*";
+            else{
+                if (j == 1)
+                    cout << "*";
+                else if (j == sizeOfSquare)
+                    cout << "*";
+                else
+                    cout << " ";
+            }
+
+        }
+    cout << "*" << endl;
+    }
+    return 0;
+}
