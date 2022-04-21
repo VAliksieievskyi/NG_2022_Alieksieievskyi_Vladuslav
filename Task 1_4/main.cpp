9f66c1024c9f5a92bbff0495f1043d910d05f3e7
Vlad_Alieksieievskyi <xr940kov@gmail.com>
2022-04-21 18:47:38 +0300

    Correct Task 1_2, Updated Task 1_4

--- Task 1_4/main.cpp
+++ Task 1_4/main.cpp
@@ -13,11 +13,8 @@ int main()
     cout << "CPU: ";
     cin >> cpuPrice;
     cout << "Enter your discount:(%) " ;
-    int discountPercent = 0, discount = 0, pcPrice = 0, allPrice = 0;
-    cin >> discountPercent;
-    pcPrice = motherboardPrice + videocardPrice + cpuPrice;
-    discount = pcPrice / 100 * discountPercent ;
-    allPrice = pcPrice - discount;
-    cout << "Your PC will cost: " << allPrice<<"$" << endl;
+    int discount = 0;
+    cin >> discount;
+    cout << "Your PC will cost: " << motherboardPrice + videocardPrice + cpuPrice - ((motherboardPrice + videocardPrice + cpuPrice) / 100 * discount)<<"$" << endl;
     return 0;
 }
