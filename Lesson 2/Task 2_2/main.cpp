28604655240c3998d650919324a56095025db1f0
Vlad_Alieksieievskyi <xr940kov@gmail.com>
2022-04-28 20:47:08 +0300

    Added function which count pair numbers

--- Lesson 2/Task 2_2/main.cpp
+++ Lesson 2/Task 2_2/main.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter your hash-code" << endl;
-    int hashCode = 0;
-    cin >> hashCode;
-    for (int i = 0; i )
+    cout << "Enter your number" << endl;
+    long int number = 0,endNumber = 0;
+    cin >> number;
+    do{
+        if ((number % 2) == 0){
+           endNumber += (number % 10);
+        }
+    }while ((number /= 10) > 0);
+    cout << "The number was:" << endNumber << endl;
     return 0;
 }
